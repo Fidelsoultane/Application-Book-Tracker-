1c1aa61783a00a5e7b3ba6a831a750eff61c9df0
Fidelsoultane <sultan620@gmail.com>
2025-02-20 11:38:48 +0100

    update css

--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.html", "./public/**/*.js"],  // Ajuste selon l'emplacement de tes fichiers
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
